63ea5f8bcc1811bde00c185a707db2ef57641163
Charles <someone@example.com>
2026-07-14 20:31:25 +0000

    improved data processing

--- _build_fpga/led_test.v
+++ _build_fpga/led_test.v
@@ -12,7 +12,7 @@ module led_counter (
 
     wire [3:0] _12;
     wire _13;
-    wire [23:0] _79;
+    wire [23:0] _76;
     wire [23:0] _14;
     wire [23:0] _19;
     wire [23:0] _16;
@@ -20,12 +20,16 @@ module led_counter (
     wire [23:0] _21;
     wire [23:0] _2;
     reg [23:0] _15;
-    wire _80;
-    wire _81;
-    wire [5:0] _78;
-    wire [14:0] _74;
-    wire [31:0] _54;
-    wire [14:0] _65;
+    wire _77;
+    wire _78;
+    wire [5:0] _75;
+    wire _72;
+    wire _71;
+    wire _70;
+    wire _69;
+    wire _68;
+    wire [31:0] _53;
+    wire [11:0] _64;
     wire vdd;
     wire [3:0] _29;
     wire [3:0] _10;
@@ -39,57 +43,50 @@ module led_counter (
     wire _30;
     wire _32;
     wire gnd;
-    wire [27:0] _51;
+    wire [27:0] _50;
     wire [1:0] _37;
     wire [1:0] _35;
     wire [1:0] _36;
     wire [1:0] _38;
-    wire [11:0] _34;
-    wire [13:0] _39;
+    wire [17:0] _34;
+    wire [19:0] _39;
     wire _40;
     wire [1:0] _41;
     wire [3:0] _42;
     wire [7:0] _43;
-    wire [15:0] _44;
-    wire [17:0] _45;
+    wire [11:0] _44;
+    wire [31:0] _45;
     wire [31:0] _46;
-    wire [31:0] _47;
-    wire _48;
-    wire [1:0] _49;
-    wire [3:0] _50;
+    wire _47;
+    wire [1:0] _48;
+    wire [3:0] _49;
+    wire [31:0] _51;
     wire [31:0] _52;
-    wire [31:0] _53;
     wire [31:0] _6;
     reg [31:0] _33;
-    wire [63:0] _56;
+    wire [63:0] _55;
+    wire [31:0] _56;
     wire [31:0] _57;
-    wire [31:0] _58;
-    wire _59;
-    wire [1:0] _60;
-    wire [3:0] _61;
-    wire [7:0] _62;
-    wire [15:0] _63;
-    wire [16:0] _64;
+    wire _58;
+    wire [1:0] _59;
+    wire [3:0] _60;
+    wire [7:0] _61;
+    wire [15:0] _62;
+    wire [19:0] _63;
+    wire [31:0] _65;
     wire [31:0] _66;
-    wire [31:0] _67;
     wire [31:0] _7;
-    reg [31:0] _55;
-    wire _68;
-    wire [1:0] _69;
-    wire [3:0] _70;
-    wire [7:0] _71;
-    wire [15:0] _72;
-    wire [16:0] _73;
-    wire [31:0] _75;
-    wire [5:0] _76;
-    wire [5:0] _77;
-    reg [5:0] _82;
+    reg [31:0] _54;
+    wire _67;
+    wire [5:0] _73;
+    wire [5:0] _74;
+    reg [5:0] _79;
     assign _12 = 4'b0100;
     assign _13 = _11 < _12;
-    assign _79 = 24'b000000000000000000000001;
+    assign _76 = 24'b000000000000000000000001;
     assign _14 = 24'b000000000000000000000000;
-    assign _19 = _15 + _79;
-    assign _16 = 24'b000101001001100101101111;
+    assign _19 = _15 + _76;
+    assign _16 = 24'b000000100000111101010111;
     assign _17 = _15 == _16;
     assign _21 = _17 ? _14 : _19;
     assign _2 = _21;
@@ -99,14 +96,18 @@ module led_counter (
         else
             _15 <= _2;
     end
-    assign _80 = _15 == _79;
-    assign _81 = _80 ? vdd : gnd;
-    assign _78 = 6'b000000;
-    assign _74 = _55[31:17];
-    assign _54 = 32'b00000000000000000000000000000000;
-    assign _65 = _58[31:17];
+    assign _77 = _15 == _76;
+    assign _78 = _77 ? vdd : gnd;
+    assign _75 = 6'b000000;
+    assign _72 = _54[28:28];
+    assign _71 = _54[26:26];
+    assign _70 = _54[24:24];
+    assign _69 = _54[22:22];
+    assign _68 = _54[20:20];
+    assign _53 = 32'b00000000000000000000000000000000;
+    assign _64 = _57[31:20];
     assign vdd = 1'b1;
-    assign _29 = 4'b0010;
+    assign _29 = 4'b0111;
     assign _10 = 4'b0000;
     assign _24 = 4'b0001;
     assign _25 = _11 + _24;
@@ -123,15 +124,15 @@ module led_counter (
     assign _30 = _11 == _29;
     assign _32 = _30 ? gnd : vdd;
     assign gnd = 1'b0;
-    assign _51 = _47[31:4];
+    assign _50 = _46[31:4];
     assign _37 = 2'b01;
     assign _35 = { pdm_dat,
                    pdm_dat };
     assign _36 = _35 + _35;
     assign _38 = _36 - _37;
-    assign _34 = 12'b010000000000;
+    assign _34 = 18'b000001000000000000;
     assign _39 = $signed(_34) * $signed(_38);
-    assign _40 = _39[13:13];
+    assign _40 = _39[19:19];
     assign _41 = { _40,
                    _40 };
     assign _42 = { _41,
@@ -139,32 +140,32 @@ module led_counter (
     assign _43 = { _42,
                    _42 };
     assign _44 = { _43,
-                   _43 };
+                   _42 };
     assign _45 = { _44,
-                   _41 };
-    assign _46 = { _45,
                    _39 };
-    assign _47 = _46 - _33;
-    assign _48 = _47[31:31];
+    assign _46 = _45 - _33;
+    assign _47 = _46[31:31];
+    assign _48 = { _47,
+                   _47 };
     assign _49 = { _48,
                    _48 };
-    assign _50 = { _49,
-                   _49 };
-    assign _52 = { _50,
-                   _51 };
-    assign _53 = _33 + _52;
-    assign _6 = _53;
+    assign _51 = { _49,
+                   _50 };
+    assign _52 = _33 + _51;
+    assign _6 = _52;
     always @(posedge clock) begin
         if (gnd)
-            _33 <= _54;
+            _33 <= _53;
         else
             if (_32)
                 _33 <= _6;
     end
-    assign _56 = $signed(_33) * $signed(_33);
-    assign _57 = _56[31:0];
-    assign _58 = _57 - _55;
-    assign _59 = _58[31:31];
+    assign _55 = $signed(_33) * $signed(_33);
+    assign _56 = _55[31:0];
+    assign _57 = _56 - _54;
+    assign _58 = _57[31:31];
+    assign _59 = { _58,
+                   _58 };
     assign _60 = { _59,
                    _59 };
     assign _61 = { _60,
@@ -172,43 +173,34 @@ module led_counter (
     assign _62 = { _61,
                    _61 };
     assign _63 = { _62,
-                   _62 };
-    assign _64 = { _63,
-                   _59 };
-    assign _66 = { _64,
-                   _65 };
-    assign _67 = _55 + _66;
-    assign _7 = _67;
+                   _60 };
+    assign _65 = { _63,
+                   _64 };
+    assign _66 = _54 + _65;
+    assign _7 = _66;
     always @(posedge clock) begin
         if (gnd)
-            _55 <= _54;
+            _54 <= _53;
         else
             if (_32)
-                _55 <= _7;
+                _54 <= _7;
     end
-    assign _68 = _55[31:31];
-    assign _69 = { _68,
-                   _68 };
-    assign _70 = { _69,
-                   _69 };
-    assign _71 = { _70,
-                   _70 };
-    assign _72 = { _71,
-                   _71 };
-    assign _73 = { _72,
-                   _68 };
-    assign _75 = { _73,
-                   _74 };
-    assign _76 = _75[5:0];
-    assign _77 = ~ _76;
+    assign _67 = _54[18:18];
+    assign _73 = { _67,
+                   _68,
+                   _69,
+                   _70,
+                   _71,
+                   _72 };
+    assign _74 = ~ _73;
     always @(posedge clock) begin
         if (gnd)
-            _82 <= _78;
+            _79 <= _75;
         else
-            if (_81)
-                _82 <= _77;
+            if (_78)
+                _79 <= _74;
     end
-    assign leds = _82;
+    assign leds = _79;
     assign pdm_clk = _13;
 
 endmodule
--- blink.ml
+++ blink.ml
@@ -27,7 +27,7 @@ let sample_hold input ~enable =
 
 
 let read_flag = 
-  mux2 (enable_counter ==:. 2) gnd vdd
+  mux2 (enable_counter ==:. 7) gnd vdd
 
 let pdm_clk = Hardcaml.Signal.output "pdm_clk" (enable_counter <:. 4)
 
@@ -56,23 +56,36 @@ let divide input ~divisor_power =
    (spec) 
    ~enable:vdd
    ~width:24                           
-   ~f:(mod_counter ~max:(2700000 / 2 - 1))
+   ~f:(mod_counter ~max:(270000 / 2 - 1))
 
 let display_flag = 
   mux2 (display_counter ==:. 1) vdd gnd
-  
+
+(* let select_bits input high width step = 
+    ((List.map (bit input))
+    (List.map(( + ) high))
+    (List.init width (( * ) step)))
+    |> concat_msb
+*)
+
+let select_list input  =
+        [bit input 18; bit input 20; bit input 22; bit input 24; bit input 26; bit input 28]
+        |> concat_msb
 
 let envelope = 
         pdm_dat 
         |> sresize ~width:2
-        |> centre_amp ~gain:(of_signed_int ~width:12 (1024))
+        |> centre_amp ~gain:(of_signed_int ~width:18 (4096))
         |> sresize ~width:32
         |> lpf ~divisor_power:4
         |> rectify
-        |> sresize ~width:32
-        |> lpf ~divisor_power:17
+        |> sresize ~width:32 
+        |> lpf ~divisor_power:20
+        |> select_list
+(*        |> (.:+[]) (25, (Some 6))
         |> divide ~divisor_power:17 (* don't do this like this *)
         |> sresize ~width:6
+*)
         |> invert 
         |> sample_hold ~enable:display_flag
 
@@ -106,16 +119,16 @@ let multi_cycle simulator n =
         done
 
 let build () =
-        (*Note reversed order. AIUI the order of evaluation is technically undefined. Maybe I should be mapping over a list of commands*)
-        ( 
-        Sys.command "gowin_pack -d GW2A-18C -o _build_fpga/pack.fs _build_fpga/pnr_led_test.json",
-        Sys.command "yowasp-nextpnr-himbaechel-gowin --json _build_fpga/led_test.json --write _build_fpga/pnr_led_test.json --device $DEVICE --vopt family=GW2A-18C --vopt cst=fpga/mic_test.cst",
-        Sys.command "yosys fpga/synth.ys"
-)
+        (* Short-circuit AND means that subsequent commands do not get run if one fails*)
+        Sys.command "yosys fpga/synth.ys" == 0
+        &&
+        Sys.command "yowasp-nextpnr-himbaechel-gowin --json _build_fpga/led_test.json --write _build_fpga/pnr_led_test.json --device $DEVICE --vopt family=GW2A-18C --vopt cst=fpga/mic_test.cst" == 0
+        &&
+        Sys.command "gowin_pack -d GW2A-18C -o _build_fpga/pack.fs _build_fpga/pnr_led_test.json" == 0
         
 
-let flash() = Sys.command "openFPGALoader _build_fpga/pack.fs"
+let flash() = Sys.command "openFPGALoader _build_fpga/pack.fs" == 0
 
-let build_and_flash () = (flash(), build(), write_circuit())
+let build_and_flash () = (build() && flash(), write_circuit())
 
 
